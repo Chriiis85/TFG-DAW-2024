055e3f37b2a2588b07578e455646c45847dcf683
Christian Moreno Diaz <krizmoreno85@gmail.com>
2024-04-15 19:58:46 +0200

    ArregladoProblemasDeVisibilidadyRutas

--- JS/script.js
+++ JS/script.js
@@ -18,3 +18,33 @@ function handleContentVisibility() {
 window.addEventListener("scroll", handleContentVisibility);
 
 
+$(document).ready(function () {
+  $("#header-drop-container-formulaone").hide(); // Ocultar el contenido al cargar la página
+
+  var timeout; // Variable para almacenar el temporizador
+
+  $("#formulaone, #header-drop-container-formulaone").hover(
+    function () {
+      clearTimeout(timeout); // Limpiar el temporizador para evitar cierres automáticos
+      $("#header-drop-container-formulaone")
+        .stop()
+        .slideDown(function () {
+          $(this).css("display", "flex"); // Cambiar a display: flex cuando se completa la animación slideDown
+        }); // Mostrar el contenido con animación slideDown
+    },
+    function () {
+      // Iniciar un temporizador para cerrar el contenido después de 500ms (medio segundo)
+      timeout = setTimeout(function () {
+        $("#header-drop-container-formulaone").stop().slideUp(); // Ocultar el contenido con animación slideUp
+      }, 500);
+    }
+  );
+
+  // Cancelar el cierre si se pasa el ratón sobre el contenido mientras el temporizador está activo
+  $("#header-drop-container-formulaone").mouseenter(function () {
+    clearTimeout(timeout); // Limpiar el temporizador
+  });
+});
+
+
+
