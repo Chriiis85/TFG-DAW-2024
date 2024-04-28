804816edac517e226e90b9c286c8a91272e9ed5d
Christian Moreno Diaz <krizmoreno85@gmail.com>
2024-04-28 19:48:33 +0200

    TeamsCSSRetocado

--- teams.php
+++ teams.php
@@ -7,8 +7,8 @@
   <title>Constructors - Motoring Community</title>
   <link rel="stylesheet" href="CSS/teams.css">
   <link rel="stylesheet" href="CSS/footer.css">
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <script defer src="JS/script.js"></script>
+  <!--<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>-->
+  <!--<script defer src="JS/script.js"></script>-->
 </head>
 
 <body>
@@ -16,9 +16,6 @@
   include "header.php";
   ?>
   <section class="main">
-    <article class="teams-title-container">
-      <h1 class="teams-title">2024 Formula One Constructors</h1>
-    </article>
     <article class="teams-container">
       <?php
       include ("PHP/driverTeam.php");
@@ -29,15 +26,13 @@
         // Reemplazar espacios en el nombre del equipo para evitar problemas con los nombres de archivos
         $nombre_equipo = str_replace(' ', '', $clasificacionesEquipos[$i]['Constructor']['name']);
 
-        // Imprimir el contenedor de la tarjeta del equipo
-        echo '<div class="team-card-container" style="border:solid 5px var(--' . $nombre_equipo . ');   border-left: none;
-              border-bottom: none;">
-              <div class="team-card">
-                <div class="team-card-title">
-                <h1>' . $clasificacionesEquipos[$i]['Constructor']['name'] . '</h1>            
-                <div> <img src="Images/Teams/' . $nombre_equipo . '.png" alt=""></div>
-                </div>
-                <div class="team-card-divider"></div>';
+        echo '<div class="team-card-container" style="border:solid 5px var(--' . $nombre_equipo . ');border-left: none;
+        border-bottom: none;">
+        <div class="team-card-title">
+        <h1>' . $clasificacionesEquipos[$i]['Constructor']['name'] . '</h1>
+          <img src="Images/Teams/' . $nombre_equipo . '.png" alt="">
+        </div>';
+
 
         // Iterar sobre los pilotos del equipo (solo los primeros dos)
         for ($j = 0; $j < 2; $j++) {
@@ -49,26 +44,36 @@
 
           // Obtener el apellido del piloto usando substr desde el primer espacio hasta el final
           $apellido = substr($nombre_completo, $primerEspacio + 1);
-          // Imprimir la tarjeta del piloto
-          echo ' <div class="team-card-drivers">
-                          <div class="team-card-driver-container">
-                              <div class="team-card-driver">
-                                  <h1>' . $nombre_completo . '</h1>
-                                  <img src="Images/Drivers/' . $apellido . '.png" alt="">
-                              </div>
-                          </div>
-                      </div>';
-        }
 
-        // Imprimir el divisor y la imagen del auto del equipo
-        echo '<div class="team-card-divider2"></div>
-                          <div class="team-card-cars">
-                              <img src="Images/Cars/' . $nombre_equipo . '.png" alt="">
-                          </div>
-                      </div>
-                  </div>';
+          echo '<div class="team-card-driver">
+          <h1>' . $nombre_completo . '</h1>
+          <img src="Images/Drivers/' . $apellido . '.png" alt="">
+            </div>';
+        }
+        echo '      <div class="team-card-car">
+                  <img src="Images/Cars/' . $nombre_equipo . '.png" alt="">
+                  </div>
+              </div>';
       }
       ?>
+      <!--<div class="team-card-container">
+        <div class="team-card-title">
+          <h1>Ferrari</h1>
+          <img src="Images/Teams/Ferrari.png" alt="">
+        </div>
+        <div class="team-card-driver">
+          <h1>Carlos Sainz</h1>
+          <img src="Images/Drivers/Sainz.png" alt="">
+        </div>
+        <div class="team-card-driver">
+          <h1>Carlos Sainz</h1>
+          <img src="Images/Drivers/Sainz.png" alt="">
+        </div>
+        <div class="team-card-car">
+          <img src="Images/Cars/Ferrari.png" alt="">
+        </div>
+      </div>-->
+
     </article>
   </section>
   <footer>
