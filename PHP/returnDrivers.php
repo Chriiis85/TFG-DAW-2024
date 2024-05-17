feea8a80618110c554ef01b72a4857436b938efc
Christian Moreno Diaz <krizmoreno85@gmail.com>
2024-05-17 20:38:18 +0200

    DriversHecho

---
+++ PHP/returnDrivers.php
@@ -0,0 +1,19 @@
+<?php
+// URL de la API
+$url = 'https://ergast.com/api/f1/2024/driverStandings.json';
+
+// Obtener los datos JSON de la API
+$data = file_get_contents($url);
+
+// Decodificar los datos JSON en un array PHP
+$resultado = json_decode($data, true);
+
+// Verificar si se obtuvo una respuesta válida
+if ($resultado && isset($resultado['MRData']['StandingsTable']['StandingsLists'][0]['DriverStandings'])) {
+    // Obtener la lista de clasificaciones de pilotos
+    $clasificacionesPilotos = $resultado['MRData']['StandingsTable']['StandingsLists'][0]['DriverStandings'];
+    
+} else {
+    echo "No se pudo obtener la información de la API.";
+}
+?>
--- drivers.php
+++ drivers.php
@@ -15,19 +15,55 @@
 
 <body>
   <?php
-  include "header.php"
-    ?>
+  include "header.php";
+  include "PHP/returnDrivers.php";
+  ?>
 
   <section class="main">
-    <article class="drivers-title-container">
+    <!--<article class="drivers-title-container">
       <h1 class="drivers-title">2024 Formula One Drivers</h1>
-    </article>
+    </article>-->
     <article class="card-driver-container">
 
+    <?php
+    for ($i=0; $i < sizeof($clasificacionesPilotos); $i++) { 
+      $team = $clasificacionesPilotos[$i]["Constructors"][0]["name"];
+      $team  =str_replace(' ', '', $team );
+      echo '
+      <div class="card-driver" style=" 
+          box-shadow: 0 0 2px black, 0 0 10px black, 0 0 20px var(--'.$team.'), 0 0 30px var(--'.$team.'), 
+          0 0 40px var(--'.$team.'), 0 0 50px var(--'.$team.'); 
+          background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(\'https://media.formula1.com/content/dam/fom-website/manual/Helmets2024/'.$clasificacionesPilotos[$i]["Driver"]["familyName"].'.png\');
+      ">';
+     echo'<div class="driver-image" >
+       <img src="Images/DriversPNG/'.$clasificacionesPilotos[$i]["Driver"]["familyName"].'.png" alt="">
+     </div>
+     <div class="card-driver-info">
+       <div class="driverNumber">
+         <h1>'.$clasificacionesPilotos[$i]["Driver"]["permanentNumber"].'</h1>
+         <h1>'.$clasificacionesPilotos[$i]["Driver"]["code"].'</h1>
+       </div>
+       <div class="driver-data">
+         <div class="driverHelmet">
+           <img src="https://media.formula1.com/content/dam/fom-website/manual/Helmets2024/'.$clasificacionesPilotos[$i]["Driver"]["familyName"].'.png" alt="">
+         </div>
+         <div class="driverFlag">
+           <img src="https://media.formula1.com/content/dam/fom-website/flags/'.nacionalidadAPais($clasificacionesPilotos[$i]["Driver"]["nationality"]).'.jpg" alt="">
+         </div>
+       </div>
+     </div>
+     <div class="card-driver-name">
+       <h1 class="name">'.$clasificacionesPilotos[$i]["Driver"]["givenName"].'</h1>
+       <h1 class="surname">'.$clasificacionesPilotos[$i]["Driver"]["familyName"].'</h1>
+     </div>
+   </div>
+     ';
+    }
+    ?>
     </article>
   </section>
   <?php
-    include "footer.php";
+  include "footer.php";
   ?>
 </body>
 <script>
@@ -36,7 +72,66 @@
 
   let headerContainer = document.getElementById("header-container");
   headerContainer.style.backgroundImage = "url('https://cdn.topgear.es/sites/navi.axelspringer.es/public/media/image/2023/03/fernando-alonso-aston-martin-f1-2975194.jpg?tf=1200x')";
-
 </script>
-
+<?php
+function nacionalidadAPais($nacionalidad)
+{
+  switch ($nacionalidad) {
+    case 'Dutch':
+      return 'Netherlands';
+      break;
+    case 'Mexican':
+      return 'Mexico';
+      break;
+    case 'Monegasque':
+      return 'Monaco';
+      break;
+    case 'Spanish':
+      return 'Spain';
+      break;
+    case 'British':
+      return 'Great-Britain';
+      break;
+    case 'Australian':
+      return 'Australia';
+      break;
+    case 'Canadian':
+      return 'Canada';
+      break;
+    case 'Japanese':
+      return 'Japan';
+      break;
+    case 'German':
+      return 'Germany';
+      break;
+    case 'Danish':
+      return 'Denmark';
+      break;
+    case 'Chinese':
+      return 'China';
+      break;
+    case 'French':
+      return 'France';
+      break;
+    case 'Finnish':
+      return 'Finland';
+      break;
+    case 'American':
+      return 'United%20States';
+      break;
+    case 'Thai':
+      return 'Thailand';
+      break;
+    case 'Italian':
+      return 'Italy';
+      break;
+    case 'Austrian':
+      return 'Austria';
+      break;
+    case 'Swiss':
+      return 'Switzerland';
+      break;
+  }
+}
+?>
 </html>
