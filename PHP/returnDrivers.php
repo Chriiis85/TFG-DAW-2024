74d5f16ca1fbc68772b1791d02cbf0929b6a7763
Christian Moreno Diaz <krizmoreno85@gmail.com>
2024-05-21 20:38:03 +0200

    Revisado

--- PHP/returnDrivers.php
+++ PHP/returnDrivers.php
@@ -1,19 +1,81 @@
 <?php
-// URL de la API
+//URL DE LA API PARA RECOGER LA CLASIFIACION DE LOS PILOTOS
 $url = 'https://ergast.com/api/f1/2024/driverStandings.json';
 
-// Obtener los datos JSON de la API
+//OBTENER EN UN ARRAY LOS DATOS DE LA API
 $data = file_get_contents($url);
 
-// Decodificar los datos JSON en un array PHP
+// DECODIFICAR LOS DATOS EN JSON PARA PINTARLOS EN LA PAGINA
 $resultado = json_decode($data, true);
 
-// Verificar si se obtuvo una respuesta válida
+// VERIFICAR SI SE OBTIENE UNA RESPUESTA
 if ($resultado && isset($resultado['MRData']['StandingsTable']['StandingsLists'][0]['DriverStandings'])) {
-    // Obtener la lista de clasificaciones de pilotos
+    // OBTENER EN UN ARRAY EL RESULTADO QUE TENEMOS
     $clasificacionesPilotos = $resultado['MRData']['StandingsTable']['StandingsLists'][0]['DriverStandings'];
     
 } else {
+    //SALIDA EN CASO DE ERROR
     echo "No se pudo obtener la información de la API.";
+}
+
+/*FUNCION QUE NOS DEVUELVE EL PAIS DE SU GENTILICIO PARA PODER COGER LA BANDERA DE LA API*/
+function nacionalidadAPais($nacionalidad)
+{
+  switch ($nacionalidad) {
+    case 'Dutch':
+      return 'Netherlands';
+      break;
+    case 'Mexican':
+      return 'Mexico';
+      break;
+    case 'Monegasque':
+      return 'Monaco';
+      break;
+    case 'Spanish':
+      return 'Spain';
+      break;
+    case 'British':
+      return 'Great-Britain';
+      break;
+    case 'Australian':
+      return 'Australia';
+      break;
+    case 'Canadian':
+      return 'Canada';
+      break;
+    case 'Japanese':
+      return 'Japan';
+      break;
+    case 'German':
+      return 'Germany';
+      break;
+    case 'Danish':
+      return 'Denmark';
+      break;
+    case 'Chinese':
+      return 'China';
+      break;
+    case 'French':
+      return 'France';
+      break;
+    case 'Finnish':
+      return 'Finland';
+      break;
+    case 'American':
+      return 'United%20States';
+      break;
+    case 'Thai':
+      return 'Thailand';
+      break;
+    case 'Italian':
+      return 'Italy';
+      break;
+    case 'Austrian':
+      return 'Austria';
+      break;
+    case 'Swiss':
+      return 'Switzerland';
+      break;
+  }
 }
 ?>
--- drivers.php
+++ drivers.php
@@ -4,9 +4,12 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Drivers - Motoring Community</title>
+  <title>F1 Drivers - Motoring Community</title>
+  <!--SCRIPT JQUERY-->
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <!--SCRIPT PRINCIPAL PAGINA-->
   <script defer src="JS/script.js"></script>
+  <!--HOJA DE ESTILOS DRIVERS-->
   <link rel="stylesheet" href="CSS/drivers.css">
 </head>
 
@@ -19,11 +22,10 @@
   ?>
   <!--BOTON PARA VOLVER ARRIBA DE LA PAGINA-->
   <button onclick="scrollToTop()" id="upBtn" class="up-button">
-    <img src="Images/UPARROW.svg" alt="" />
+    <img src="Images/UPARROW.svg" alt="Up Arrow" />
   </button>
   <section class="main">
     <article class="card-driver-container">
-
       <?php
       /*BUCLE QUE EN SU ITERACION SE CREAN LAS TARJETAS Y SE MUESTRA LA INFORMACION DEL ARRAY*/
       for ($i = 0; $i < sizeof($clasificacionesPilotos); $i++) {
@@ -36,7 +38,7 @@
           background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(\'https://media.formula1.com/content/dam/fom-website/manual/Helmets2024/' . $clasificacionesPilotos[$i]["Driver"]["familyName"] . '.png\');
       ">';
         echo '<div class="driver-image" >
-       <img src="Images/DriversPNG/' . $clasificacionesPilotos[$i]["Driver"]["familyName"] . '.png" alt="">
+       <img src="Images/DriversPNG/' . $clasificacionesPilotos[$i]["Driver"]["familyName"] . '.png" alt="Driver Image">
      </div>
      <div class="card-driver-info">
        <div class="driverNumber">
@@ -45,10 +47,10 @@
        </div>
        <div class="driver-data">
          <div class="driverHelmet">
-           <img src="https://media.formula1.com/content/dam/fom-website/manual/Helmets2024/' . $clasificacionesPilotos[$i]["Driver"]["familyName"] . '.png" alt="">
+           <img src="https://media.formula1.com/content/dam/fom-website/manual/Helmets2024/' . $clasificacionesPilotos[$i]["Driver"]["familyName"] . '.png" alt="Driver Helmet">
          </div>
          <div class="driverFlag">
-           <img src="https://media.formula1.com/content/dam/fom-website/flags/' . nacionalidadAPais($clasificacionesPilotos[$i]["Driver"]["nationality"]) . '.jpg" alt="">
+           <img src="https://media.formula1.com/content/dam/fom-website/flags/' . nacionalidadAPais($clasificacionesPilotos[$i]["Driver"]["nationality"]) . '.jpg" alt="Country Flag">
          </div>
        </div>
      </div>
@@ -62,80 +64,18 @@
       ?>
     </article>
   </section>
-    <!--IMPLEMENTAR FOOTER IGUAL QUE EN TODAS LAS PAGINAS-->
+  <!--IMPLEMENTAR FOOTER IGUAL QUE EN TODAS LAS PAGINAS-->
   <?php
   include "footer.php";
   ?>
 </body>
 <script>
-    //SCRIPT QUE DEFINE EL TITULO DE LA PAGINA EN EL HEADER Y ESTABLECE LA IMAGEN DE FONDO DE LA CABECERA-->
+  //SCRIPT QUE DEFINE EL TITULO DE LA PAGINA EN EL HEADER Y ESTABLECE LA IMAGEN DE FONDO DE LA CABECERA-->
   let tituloPrincipal = document.getElementById("title-header");
   tituloPrincipal.textContent = "Formula One 2024 Drivers";
 
   let headerContainer = document.getElementById("header-container");
   headerContainer.style.backgroundImage = "url('https://cdn.topgear.es/sites/navi.axelspringer.es/public/media/image/2023/03/fernando-alonso-aston-martin-f1-2975194.jpg?tf=1200x')";
 </script>
-<?php
-/*FUNCION QUE NOS DEVUELVE EL PAIS DE SU GENTILICIO PARA PODER COGER LA BANDERA DE LA API*/
-function nacionalidadAPais($nacionalidad)
-{
-  switch ($nacionalidad) {
-    case 'Dutch':
-      return 'Netherlands';
-      break;
-    case 'Mexican':
-      return 'Mexico';
-      break;
-    case 'Monegasque':
-      return 'Monaco';
-      break;
-    case 'Spanish':
-      return 'Spain';
-      break;
-    case 'British':
-      return 'Great-Britain';
-      break;
-    case 'Australian':
-      return 'Australia';
-      break;
-    case 'Canadian':
-      return 'Canada';
-      break;
-    case 'Japanese':
-      return 'Japan';
-      break;
-    case 'German':
-      return 'Germany';
-      break;
-    case 'Danish':
-      return 'Denmark';
-      break;
-    case 'Chinese':
-      return 'China';
-      break;
-    case 'French':
-      return 'France';
-      break;
-    case 'Finnish':
-      return 'Finland';
-      break;
-    case 'American':
-      return 'United%20States';
-      break;
-    case 'Thai':
-      return 'Thailand';
-      break;
-    case 'Italian':
-      return 'Italy';
-      break;
-    case 'Austrian':
-      return 'Austria';
-      break;
-    case 'Swiss':
-      return 'Switzerland';
-      break;
-  }
-}
-?>
 
 </html>
