a27611663f81247a176d7e0d9e5e3f6e0a916ec1
Christian Moreno Diaz <krizmoreno85@gmail.com>
2024-05-28 18:36:06 +0200

    Comentado y Revisado

--- PHP/Forum/search_post.php
+++ PHP/Forum/search_post.php
@@ -1,38 +1,52 @@
 <?php
-// Recibimos los datos del formulario desde el AJAX
+//RECOGEMOS DEL INPUT LAS LETRAS O CADENA
 $letra = $_POST['letra'];
+
+//OBTENEMOS EL ID DEL TEMA EN EL QUE ESTA EL POST
 $id_theme = $_POST['id_theme'];
 
-// Verificar si los datos son correctos en la BBDD
+//INCLUIR EL ARCHIVODE LA CONEXION
 include "conexion.php";
-include "returnPosts.php";
-//Probar la conexión
-if (!$con->connect_error) {
-    $consulta = "SELECT * FROM posts WHERE contenido LIKE ? AND id_theme = ?";
-    $stmt = $con->prepare($consulta);
-    $like_letra = "%" . $letra . "%";
-    $stmt->bind_param("si", $like_letra, $id_theme); // Assuming $id_theme is defined and is an integer
 
-    // Verificamos si se ejecuta bien la consulta.
+//INCLUIR EL ARCHIVO QUE DEVUELVE LOS POSTS PARA VOLVER A PINTARLOS
+include "returnPosts.php";
+
+//PROBAR LA CONEXION
+if (!$con->connect_error) {
+    //CONSULTA QUE VAMOS A REALIZAR, BUSCAR POSTS QUE CONTENGAN EL STRING QUE NOS LLEGA DEL INPUT
+    $consulta = "SELECT * FROM posts WHERE contenido LIKE ? AND id_theme = ?";
+
+    //PREPARAR LA CONSULTA
+    $stmt = $con->prepare($consulta);
+
+    //AÑADIR POR PARAMETRO EL FILTRADO QUE SE VA A BUSCAR
+    $like_letra = "%" . $letra . "%";
+
+    //ENLAZAR LOS PARAMETROS
+    $stmt->bind_param("si", $like_letra, $id_theme);
+
+    //VERIFICAR SI SE EJECUTA BIEN LA CONSULTA
     if ($stmt->execute()) {
+        //OBTENER LOS RESULTADOS
         $result = $stmt->get_result();
-        // Obtener todos los resultados
         $posts = $result->fetch_all(MYSQLI_NUM);
-        
-        // Map user ID to username
+
+        //AÑADIR EL INDICE QUE RETORNA DE LA FUNCION EL NOMBRE DEL USUARIO
         for ($i = 0; $i < sizeof($posts); $i++) {
             $posts[$i][3] = returnNombreUsu($posts[$i][3]);
         }
 
-        // Devolver los resultados en formato JSON
+        //DECODIFICAR Y DEVOLVER EL ARRAY EN FORMATO JSON
         echo json_encode($posts);
     } else {
+        //MOSTRAR MANEJO DE ERRORES
         echo "Error en la consulta: " . $stmt->error;
     }
 
-    // Cerrar la conexión
+    //CERRAR LA CONEXION Y EL STATEMENT
     $stmt->close();
     mysqli_close($con);
 } else {
+    //MOSRTRAR ERRORES EN CASO DE QUE FALLE LA CONEXION
     die("Error de conexión: " . mysqli_connect_error());
 }
--- PHP/Forum/search_theme.php
+++ PHP/Forum/search_theme.php
@@ -1,33 +1,46 @@
 <?php
-// Recibimos los datos del formulario desde el AJAX
+//RECOGEMOS DEL INPUT LAS LETRAS O CADENA
 $letra = $_POST['letra'];
 
-// Verificar si los datos son correctos en la BBDD
+//INCLUIR EL ARCHIVO DE LA CONEXION
 include "conexion.php";
+
+//INCLUIR EL ARCHIVO QUE DEVUELVE LOS TEMAS PARA VOLVER A PINTARLOS
 include "returnThemes.php";
-//Probar la conexión
+
+
+//PROBAR LA CONEXION
 if (!$con->connect_error) {
+
+    //CONSULTA QUE VAMOS A REALIZAR, BUSCAR TEMAS QUE CONTENGAN EL STRING QUE NOS LLEGA DEL INPUT
     $consulta = "SELECT * FROM themes WHERE titulo_tema LIKE '%" . $letra . "%'";
+
+    //RECOGEMOS EL RESULTADO DE LA CONSULTA
     $result = mysqli_query($con, $consulta);
 
-    //Verificamos si se ejecuta bien la consulta.
+    //SI SE EJECUTA BIEN LA CONSULTA CONTINUAMOS
     if ($result) {
-        // Obtener todos los resultados
+        //OBTENER LOS TEMAS DEVUELTOS
         $themes = mysqli_fetch_all($result);
-        // Devolver los resultados en formato JSON
+        
+        //AÑADIR INDICES PARA MOSTRAR EL NOMBRE DEL USUARIO EL NUMERO DE POSTS Y EL ULTIMO POST
         for ($i = 0; $i < sizeof($themes); $i++) {
             $themes[$i][3] = returnNombreUsu($themes[$i][3]);
             $themes[$i][4] = returnNumberPosts($themes[$i][0]);
             $themes[$i][5] = returnLastPost($themes[$i][0]);
         }
+
+        //CODIFICAR EN FORMATO JSON EL ARRAY
         echo json_encode($themes);
     } else {
+        //MOSTRAR MENSAJE DE ERROR SI NO SE REALIZA LA CONSULTA 
         echo "Error en la consulta: " . mysqli_error($con);
     }
 
-    // Cerrar la conexión
+    //CERRAR LA CONEXION
     mysqli_close($con);
 } else {
+    //MOSTRAR MENSAJE DE ERROR DE CONEXION
     die("Error de conexión: " . mysqli_connect_error());
 }
 ?>
