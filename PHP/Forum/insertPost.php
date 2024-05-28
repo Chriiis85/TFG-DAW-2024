4dd60e35f0e4c22e4a672bb953647d4c8fc0457b
Christian Moreno Diaz <krizmoreno85@gmail.com>
2024-05-28 17:50:44 +0200

    Comentado y  Revisado

--- PHP/Forum/insertPost.php
+++ PHP/Forum/insertPost.php
@@ -1,16 +1,55 @@
 <?php
+//CONTENIDO DEL POST NUEVO QUE SE VA A CREAR
 $content = $_POST["content"];
+
+//NOMBRE DE USUARIO QUE VA A INSERTAR EL POST
 $username = $_POST["username"];
-$id_theme= $_POST["id_theme"];
-function returnIdUsu($id_nombre) {
+
+//ID DEL TEMA EN EL QUE SE VA A INSERTAR EL POST
+$id_theme = $_POST["id_theme"];
+
+//INCLUIR LA CONEXION A LA BASE DE DATOS
+include "conexion.php";
+
+//CONSULTA A EJECUTAR
+$consulta = "INSERT INTO posts(contenido, id_usuario, id_theme) VALUES (?, ?,?)";
+
+//INICIAR EL STATEMENT
+$stmt = mysqli_stmt_init($con);
+
+//RECOGER EL ID USUARIO POR MEDIO DE LA FUNCION QUE LO DEVUELVE POR MEDIO DEL USERNAME
+$id_usuario = returnIdUsu($username);
+
+//PREPARAR LA CONSULTA
+if (mysqli_stmt_prepare($stmt, $consulta)) {
+    //ENLAZAR LOS PARAMETROS
+    mysqli_stmt_bind_param($stmt, "sis", $content, $id_usuario, $id_theme);
+
+    //EJECUTAR EL STATEMENT
+    mysqli_stmt_execute($stmt);
+
+    //VERIFICAR SI HAN HABIDO CAMBIOS(SE HA INSERTADO ALGUN REGISTRO EN ESTE CASO MODIFICADO) PARA MOSTRAR EL MENSAJE DE ERROR O CONFIRMACION
+    if (mysqli_affected_rows($con) > 0) {
+        echo 1; //EL POST SE INSERTO CORRECTAMENTE
+    } else {
+        echo 2; //NO SE PUDO CREAR EL NUEVO POST
+    }
+
+    //CERRAR EL STATEMENT
+    mysqli_stmt_close($stmt);
+} else {
+    //MANEJO DE ERRORES EN CASO DE QUE SALGA O SE DETECTE ALGUN ERROR
+    echo "Error: No se pudo preparar la consulta. ERROR:" . mysqli_error($con);
+}
+
+//FUNCION QUE RETORNA EL ID DE USUARIO MEDIANTE EL NOMBRE DEL USUARIO
+function returnIdUsu($id_nombre)
+{
     // CONSULTA A EJECUTAR
     $consulta = "SELECT id FROM users WHERE username = ?";
-    include "conexion.php";
 
-    // VERIFICAR LA CONEXIÓN
-    if (!$con) {
-        return "Error: No se pudo conectar a la base de datos";
-    }
+    //INCLUIR LA CONEXION A LA BASE DE DATOS
+    include "conexion.php";
 
     // INICIAR EL STATEMENT
     $stmt = mysqli_stmt_init($con);
@@ -34,40 +73,10 @@ function returnIdUsu($id_nombre) {
         return $id_usuario;
     } else {
         // MANEJO DE ERRORES SI LA PREPARACIÓN DE LA CONSULTA FALLA
-        return "Error: No se pudo preparar la consulta";
+        return "Error: No se pudo preparar la consulta. ERROR:" . mysqli_error($con);
     }
 }
 
-include "conexion.php";
-
-//CONSULTA A EJECUTAR
-$consulta = "INSERT INTO posts(contenido, id_usuario, id_theme) VALUES (?, ?,?)";
-
-//INICIAR EL STATEMENT
-$stmt = mysqli_stmt_init($con);
-$id_usuario = returnIdUsu($username);
-//PREPARAR LA CONSULTA
-if (mysqli_stmt_prepare($stmt, $consulta)) {
-    //ENLAZAR LOS PARAMETROS
-    mysqli_stmt_bind_param($stmt, "sis", $content, $id_usuario, $id_theme);
-
-    //EJECUTAR EL STATEMENT
-    mysqli_stmt_execute($stmt);
-
-    // Verificar si se insertó correctamente una fila
-    if (mysqli_affected_rows($con) > 0) {
-        echo 1; // Indicar que se insertó correctamente
-    } else {
-        echo 2; // Indicar que no se pudo insertar
-    }
-
-    //CERRAR EL STATEMENT
-    mysqli_stmt_close($stmt);
-} else {
-    // Manejo de errores si la preparación de la consulta falla
-    echo "Error: No se pudo preparar la consulta";
-}
-
-// Cerrar la conexión
+//CERRAR LA CONEXION
 mysqli_close($con);
 ?>
--- PHP/Forum/insertTheme.php
+++ PHP/Forum/insertTheme.php
@@ -1,16 +1,52 @@
 <?php
+//RECOGER EL NOMBRE DEL NUEVO TEMA QUE SE VA A CREAR
 $nombre = $_POST["name"];
+
+//RECOGER EL NOMBRE DEL USUARIO QUE CREA EL NUEVO TEMA
 $username = $_POST["username"];
 
-function returnIdUsu($id_nombre) {
+//INCLUIR EL ARCHIVO DE LA CONEXION A LA BASE DE DATOS
+include "conexion.php";
+
+//CONSULTA A EJECUTAR
+$consulta = "INSERT INTO themes(titulo_tema, id_usuario) VALUES (?, ?)";
+
+//INICIAR EL STATEMENT
+$stmt = mysqli_stmt_init($con);
+
+//RECOGER EL ID DEL USUARIO POR MEDIO DEL NOMBRE DEL USUARIO QUE CREA EL TEMA
+$id_usuario = returnIdUsu($username);
+
+//PREPARAR LA CONSULTA
+if (mysqli_stmt_prepare($stmt, $consulta)) {
+    //ENLAZAR LOS PARAMETROS
+    mysqli_stmt_bind_param($stmt, "si", $nombre, $id_usuario);
+
+    //EJECUTAR EL STATEMENT
+    mysqli_stmt_execute($stmt);
+
+    //VERIFICAR SI HAN HABIDO CAMBIOS(SE HA INSERTADO ALGUN REGISTRO EN ESTE CASO MODIFICADO) PARA MOSTRAR EL MENSAJE DE ERROR O CONFIRMACION
+    if (mysqli_affected_rows($con) > 0) {
+        echo 1; //EL TEMA SE INSERTO CORRECTAMENTE
+    } else {
+        echo 2; //EL TEMA NO PUDO SER CREADO CORRECTAMENTE
+    }
+
+    //CERRAR EL STATEMENT
+    mysqli_stmt_close($stmt);
+} else {
+    //MANEJO DE ERRORES EN CASO DE QUE SALGA O SE DETECTE ALGUN ERROR
+    echo "Error: No se pudo preparar la consulta. ERROR:" . mysqli_error($con);
+}
+
+//FUNCION QUE RETORNA EL ID DE USUARIO MEDIANTE EL NOMBRE DEL USUARIO
+function returnIdUsu($id_nombre)
+{
     // CONSULTA A EJECUTAR
     $consulta = "SELECT id FROM users WHERE username = ?";
-    include "conexion.php";
 
-    // VERIFICAR LA CONEXIÓN
-    if (!$con) {
-        return "Error: No se pudo conectar a la base de datos";
-    }
+    //INCLUIR LA CONEXION A LA BASE DE DATOS
+    include "conexion.php";
 
     // INICIAR EL STATEMENT
     $stmt = mysqli_stmt_init($con);
@@ -34,40 +70,10 @@ function returnIdUsu($id_nombre) {
         return $id_usuario;
     } else {
         // MANEJO DE ERRORES SI LA PREPARACIÓN DE LA CONSULTA FALLA
-        return "Error: No se pudo preparar la consulta";
+        return "Error: No se pudo preparar la consulta. ERROR:" . mysqli_error($con);
     }
 }
 
-include "conexion.php";
-
-//CONSULTA A EJECUTAR
-$consulta = "INSERT INTO themes(titulo_tema, id_usuario) VALUES (?, ?)";
-
-//INICIAR EL STATEMENT
-$stmt = mysqli_stmt_init($con);
-$id_usuario = returnIdUsu($username);
-//PREPARAR LA CONSULTA
-if (mysqli_stmt_prepare($stmt, $consulta)) {
-    //ENLAZAR LOS PARAMETROS
-    mysqli_stmt_bind_param($stmt, "si", $nombre, $id_usuario);
-
-    //EJECUTAR EL STATEMENT
-    mysqli_stmt_execute($stmt);
-
-    // Verificar si se insertó correctamente una fila
-    if (mysqli_affected_rows($con) > 0) {
-        echo 1; // Indicar que se insertó correctamente
-    } else {
-        echo 2; // Indicar que no se pudo insertar
-    }
-
-    //CERRAR EL STATEMENT
-    mysqli_stmt_close($stmt);
-} else {
-    // Manejo de errores si la preparación de la consulta falla
-    echo "Error: No se pudo preparar la consulta";
-}
-
-// Cerrar la conexión
+//CERRAR LA CONEXION
 mysqli_close($con);
 ?>
