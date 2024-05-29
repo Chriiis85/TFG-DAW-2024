c600d716cb823f602ad829ef5183ea6ba5a9a752
Christian Moreno Diaz <krizmoreno85@gmail.com>
2024-05-29 13:00:22 +0200

    Accesibilidad

--- header.php
+++ header.php
@@ -18,19 +18,19 @@
   <section id="header-container" class="header-container">
     <article class="header">
       <div class="header-container1">
-        <h1 onclick="window.location.href = 'index.html'">
+        <h1 tabindex="1" onclick="window.location.href = 'index.html'">
           MOTORING COMMUNITY
         </h1>
       </div>
       <div class="header-container2">
-        <p class="progress-bar" id="formulaone">
+        <p tabindex="2" class="progress-bar" id="formulaone">
           FORMULA ONE &nbsp;&nbsp;<img src="Images/down-arrow-svgrepo-com.svg" alt="Down Arrow" />
         </p>
-        <p onclick="window.location.href = 'data.php'">
+        <p tabindex="7" onclick="window.location.href = 'data.php'">
           HISTORIC DATA
         </p>
-        <p onclick="window.location.href = 'standings.php'">SEASON STANDINGS</p>
-        <p onclick="window.location.href = 'forum.php'">FORUM</p>
+        <p tabindex="8" onclick="window.location.href = 'standings.php'">SEASON STANDINGS</p>
+        <p tabindex="9" onclick="window.location.href = 'forum.php'">FORUM</p>
       </div>
       <div class="header-container3"></div>
     </article>
@@ -41,15 +41,15 @@
         </h1>
       </div>
       <div class="header-drop-container-options">
-        <h3 onclick="window.location.href = 'aboutf1.php'"><img src="Images/down-arrow-svgrepo-com.svg"
+        <h3 tabindex="3" onclick="window.location.href = 'aboutf1.php'"><img src="Images/down-arrow-svgrepo-com.svg"
             alt="Down Arrow" />About Formula One</h3>
       </div>
       <div class="header-drop-container-options2">
-        <p onclick="window.location.href = 'drivers.php'"><img src="Images/down-arrow-svgrepo-com.svg"
+        <p tabindex="4" onclick="window.location.href = 'drivers.php'"><img src="Images/down-arrow-svgrepo-com.svg"
             alt="Down Arrow" />Season Drivers</p>
-        <p onclick="window.location.href = 'teams.php'"><img src="Images/down-arrow-svgrepo-com.svg"
+        <p tabindex="5" onclick="window.location.href = 'teams.php'"><img src="Images/down-arrow-svgrepo-com.svg"
             alt="Down Arrow" />Season Constructors</p>
-        <p onclick="window.location.href = 'calendar.php'"><img src="Images/down-arrow-svgrepo-com.svg"
+        <p tabindex="6" onclick="window.location.href = 'calendar.php'"><img src="Images/down-arrow-svgrepo-com.svg"
             alt="Down Arrow" />Season Calendar</p>
       </div>
     </div>
