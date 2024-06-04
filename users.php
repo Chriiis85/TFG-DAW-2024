baf7d6ff124813fd63b5601cbc7e47e09add8c8e
Christian Moreno Diaz <krizmoreno85@gmail.com>
2024-06-04 19:14:12 +0200

    UserS?

--- users.php
+++ users.php
@@ -21,15 +21,15 @@
       <form class="main-info-Login" role="form" method="post" action="PHP/Forum/registrar_usuario.php">
         <h1 id="tit-register">Motoring Community Register</h1>
         <div class="formField">
-          <input type="text" required="" id="mail" name="mail" />
+          <input type="text" required="" id="mail" name="mail" required/>
           <span>Username/Mail</span>
         </div>
         <div class="formField">
-          <input type="text" required="" id="name" name="name" />
+          <input type="text" required="" id="name" name="name" required/>
           <span>Name</span>
         </div>
         <div class="formField">
-          <input type="text" required="" id="surname" name="surname" />
+          <input type="text" required="" id="surname" name="surname" required/>
           <span>Surname</span>
         </div>
         <div class="formField">
@@ -42,8 +42,8 @@
         </div>
         <p id="pwdError">Pwd must contain 1 Upcase, 1 LowCase, 1 Character and at least 8 Character/Numbers</p>
         <p tabindex="0" title="Go to Register Page" onclick="window.location.href = 'users.php'" id="loginFormChange">Have an account? Login here.</p>
-        <button type="submit" id="register" class="buttons" role="button">Register</button>
-        <button type="button" title="Go to Main Page" onclick="window.location.href = 'index.html'" id="home" class="buttons" role="button">
+        <button aria-label="Register Account" type="submit" id="register" class="buttons" role="button">Register</button>
+        <button aria-label="Go Home" type="button" title="Go to Main Page" onclick="window.location.href = 'index.html'" id="home" class="buttons" role="button">
           Go Home
         </button>
       </form>
@@ -58,7 +58,7 @@
       <form class="main-info-Login" role="form" method="post" action="PHP/Forum/validar_usuario.php">
         <h1>Motoring Community Login</h1>
         <div class="formField">
-          <input id="usernameLogin" name="username" type="text" required="" />
+          <input id="usernameLogin" name="username" type="text" required />
           <span>Username</span>
         </div>
         <div class="formField">
@@ -71,41 +71,12 @@
           </button>
         </div>
         <p tabindex="0" id="registerFormChange">Don´t have an account? Register here.</p>
-        <button type="submit" id="login" class="buttons" role="button">Login</button>
-        <button title="Go to Main Page" onclick="window.location.href = 'index.html'" id="home" class="buttons" role="button">
+        <button aria-label="og Into Account" type="submit" id="login" class="buttons" role="button">Login</button>
+        <button aria-label="Go Home" title="Go to Main Page" onclick="window.location.href = 'index.html'" id="home" class="buttons" role="button">
           Go Home
         </button>
         <!--<p id="pwdFormChange">Forgot your password? Reset here.</p>-->
       </form>
-    </article>
-
-    <!--FORMULARIO PARA EL FORM DE RESETEAR LA PWD-->
-    <article id="formPwd" class="main-info-Pwd">
-      <h1>Motoring Community Reset Password</h1>
-      <div class="formField">
-        <input type="text" required="" />
-        <span>Username</span>
-      </div>
-      <div class="formField">
-        <input type="password" id="passwordField2" required />
-        <span>New Password</span>
-        <button type="button" id="togglePasswordBtn" onclick="togglePasswordVisibility2()">
-          <i id="passwordIcon" class="fa fa-eye-slash"></i>
-          <!--<img src="Images/eye_see.svg" alt="">-->
-        </button>
-      </div>
-      <div class="formField">
-        <input type="password" id="passwordField2" required />
-        <span>Confirm New Password</span>
-        <button type="button" id="togglePasswordBtn" onclick="togglePasswordVisibility2()">
-          <i id="passwordIcon" class="fa fa-eye-slash"></i>
-          <!--<img src="Images/eye_see.svg" alt="">-->
-        </button>
-      </div>
-      <button id="confirmPwd" class="buttons" role="button">Confirm</button>
-      <button title="Go to Users Page" onclick="window.location.href = 'users.php'" id="home" class="buttons" role="button">
-        Go Back
-      </button>
     </article>
   </section>
 </body>
